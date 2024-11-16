dbddb732fa9b34ccd98f4bac860c76474b87b8a3
gityb8 <statichub8@gmail.com>
2024-11-17 02:16:35 +0530

    Some Pakages Configure for prod

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
+
 const baseurl = "https://mandai-backend-api-production.up.railway.app/api"
-// const baseurl = "http://192.168.29.7:8000/api"
 
 const api = axios.create({
     baseURL:baseurl,
